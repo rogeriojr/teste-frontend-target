876fdba82f5b2b7a4e6f0361a01fdaf852dcbc18
rogeriojr <rogeriojr1100@gmail.com>
2025-01-15 02:43:49 -0300

    fix home initial route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
